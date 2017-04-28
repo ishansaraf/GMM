73c3304de6a6e191027e8a1b103d7207df071dbd
Shuruni <speedster708@gmail.com>
2017-04-27 21:10:25 -0400

    fixed merge conflict
    guh

--- src/LoginListener.java
+++ src/LoginListener.java
@@ -1,8 +1,5 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
@@ -21,17 +18,6 @@ public class LoginListener implements ActionListener {
 	public void actionPerformed(ActionEvent arg0) {
 		//Queries the Merchant Table for the Username and Password
 		//	if it finds an entry, sets the MerchantID
-<<<<<<< HEAD
-		try {
-			Statement login = Main.conn.createStatement();
-			String loginQuery = "";
-			ResultSet rs = login.executeQuery(loginQuery);
-			Main.MerchantID = rs.getString("MerchantID");
-		} catch (SQLException exception) {
-			exception.printStackTrace();
-		}
-=======
 		Main.MerchantID = "";
->>>>>>> b1c6ecaa8d656b777e25979e4721ed7c24358ff0
 	}
 }
