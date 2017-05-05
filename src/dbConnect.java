ef21cf9672df603b8a1772101b937fee7b3e7cd1
Ishan Saraf <sarafi@rose-hulman.edu>
2017-05-05 00:44:25 -0400

    Connection Ecryption
    Added encrypted config file, password is only decrypted while being used
    for the connection, and is blanked out after.

--- src/dbConnect.java
+++ src/dbConnect.java
@@ -1,19 +1,42 @@
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.properties.EncryptableProperties;
 
 public class dbConnect {
 
+	/**
+	 * Attempts to connect to the database using an encrypted configuration file
+	 * to obtain connection details. Also handles decryption and use of
+	 * encrypted configuration fields.
+	 * 
+	 * @return the connection object, null if connection is unsuccessful
+	 * @author Ishan Saraf
+	 */
 	public Connection connect() {
 		Connection conn = null;
 		try {
-			String serverURL = "jdbc:sqlserver://golem.csse.rose-hulman.edu";
-			// TODO: Figure out how to encrypt config file to ensure user+pwd
-			// data isn't visible in plaintext
-			String username = "gmm";
-			String pwd = "abc123";
+			// Creating the encryptor, and setting key used for decryption
+			StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+			encryptor.setPassword("jasyptEncrypt");
+
+			// Loading properties file used for connection
+			Properties p = new EncryptableProperties(encryptor);
+			p.load(new FileInputStream("config.properties"));
+
+			// Getting fields from properties file
+			String serverURL = p.getProperty("server");
+			String username = p.getProperty("username");
+			String pwd = p.getProperty("password");
 			String connectionURL = serverURL + ";user=" + username + ";password=" + pwd + ";databaseName=GMM";
 
 			conn = DriverManager.getConnection(connectionURL);
+			
+			// Blanking out password string
+			pwd = "";
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
