c36f7653098877e2c73399375c7026dce7db0bda
Shuruni <speedster708@gmail.com>
2017-05-12 09:29:20 -0400

    fixed spacing issue

--- src/RestockPage.java
+++ src/RestockPage.java
@@ -151,7 +151,7 @@ public class RestockPage implements GMMPage {
 		// populate ComboBox
 		List<String> tempArrList = Main.getItemList();
 		String[] modelArray = new String[tempArrList.size() + 1];
-		modelArray[0] = "                    ";
+		modelArray[0] = "                                    ";
 		for (int i = 0; i < modelArray.length - 1; i++) {
 			modelArray[i + 1] = tempArrList.get(i);
 		}
